441a72b1f0595d3b5ab28a30537cbea800bc67b0
Taras Radchenko <petuzk.dev@gmail.com>
2024-04-08 18:58:03 +0200

    Add reprs for recipes

--- src/predeq/recipes.py
+++ src/predeq/recipes.py
@@ -3,13 +3,19 @@ import re
 from ._predeq import predeq
 
 
-NOT_NONE = predeq(lambda obj: obj is not None)
+NOT_NONE = predeq(lambda obj: obj is not None, repr='<NOT_NONE>')
 
 
 def exception(exc: BaseException) -> predeq:
-    return predeq(lambda obj: isinstance(obj, type(exc)) and obj.args == exc.args)
+    return predeq(
+        lambda obj: isinstance(obj, type(exc)) and obj.args == exc.args,
+        repr=f'{exception.__name__}({exc!r})',
+    )
 
 
 def matches_re(regex) -> predeq:
     pattern = re.compile(regex)
-    return predeq(lambda obj: isinstance(obj, str) and pattern.match(obj) is not None)
+    return predeq(
+        lambda obj: isinstance(obj, str) and pattern.match(obj) is not None,
+        repr=f'{matches_re.__name__}({pattern.pattern!r})',
+    )
