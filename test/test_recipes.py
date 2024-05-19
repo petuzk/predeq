24d441e44f069fa6518076eb2855eb8b06e68ae8
Taras Radchenko <petuzk.dev@gmail.com>
2024-05-19 17:48:58 +0200

    Add missing tests for recipes

--- test/test_recipes.py
+++ test/test_recipes.py
@@ -1,16 +1,39 @@
-from predeq.recipes import NOT_NONE, exception, matches_re
+from predeq import ANY, NOT_NONE, exception, instanceof, matches_re
+
+
+def test_any():
+    assert {} == ANY
+    assert None == ANY
 
 
 def test_not_none():
     assert {} == NOT_NONE
+    assert None != NOT_NONE
 
 
 def test_exception():
-    exc = LookupError('fridge does not contain food')
+    msg = 'fridge does not contain food'
+    exc = LookupError(msg)
+
     assert exc == exception(exc)
+    assert KeyError(msg) == exception(exc)
+
+    assert ValueError() != exception(exc)
+    assert ValueError(msg) != exception(exc)
+
+
+def test_instanceof():
+    assert {} == instanceof(dict)
+    assert 123 == instanceof(int)
+    assert False == instanceof(int)
+
+    assert 'abc' != instanceof(int)
 
 
 def test_matches_re():
     pred = matches_re(r'\d{3}$')
     assert '123' == pred
     assert '1234' != pred
+
+    assert None != pred
+    assert 12.34 != pred
