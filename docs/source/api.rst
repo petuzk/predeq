753b88812606362fc8388118e2bbd2b426f9282d
Taras Radchenko <petuzk.dev@gmail.com>
2024-06-02 12:06:15 +0200

    Add basic sphinx-generated documentation

---
+++ docs/source/api.rst
@@ -0,0 +1,18 @@
+=============
+API Reference
+=============
+
+.. automodule:: predeq
+
+.. autoclass:: predeq
+
+Recipes
+=======
+
+.. autodata:: ANY
+    :no-value:
+.. autodata:: NOT_NONE
+    :no-value:
+.. autofunction:: exception
+.. autofunction:: instanceof
+.. autofunction:: matches_re
---
+++ docs/source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from importlib.metadata import version as get_version
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PredEq'
+copyright = '2024, Taras Radchenko'
+author = 'Taras Radchenko'
+
+version = release = get_version('predeq')
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
+
+pygments_style = 'sphinx'
+
+master_doc = 'toctree'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['css/overrides.css']
